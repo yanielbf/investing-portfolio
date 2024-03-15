f06a5ca9990c6cf03ef3129570cb92cf7b3d2fb7
Yaniel Blanco Fernandez <c.yblanco@shipbob.com>
2024-03-14 23:59:36 -0300

    Update

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
   console.log()
-  if (to?.name === "portfolio" && (!localStorage.getItem('hello') || localStorage.getItem('hello') !== 1234)) {
+  if (to?.name === "portfolio" && (!localStorage.getItem('hello') || localStorage.getItem('hello') !== '1234')) {
     return navigateTo("/");
   }
 });
