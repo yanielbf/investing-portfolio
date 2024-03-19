94cc02ea1f90dfe2352e24f514eb5ab472efad40
Yaniel Blanco Fernandez <c.yblanco@shipbob.com>
2024-03-18 21:33:09 -0300

    Update

--- pages/three.vue
+++ pages/three.vue
@@ -8,53 +8,7 @@ const part2 = ref();
 const part3 = ref();
 const mats = ref({});
 
-async function handler() {
-  const { nodes: part1 } = await useGLTF("/3dmodel/cover/1.glb", {
-    draco: true,
-  });
-  const { nodes: part2 } = await useGLTF("/3dmodel/cover/2.glb", {
-    draco: true,
-  });
-  const { nodes: part3 } = await useGLTF("/3dmodel/cover/3.glb", {
-    draco: true,
-  });
-
-  const group = new THREE.Group();
-  group.add(part1.mesh_0);
-  group.add(part2.mesh_0);
-  group.add(part3.mesh_0);
-  scene1.add(group);
-
-  console.log(group);
-  // scene1.remove(scene1.children[0]);
-}
-
-async function handler2() {
-  const { nodes } = await useGLTF("/3dmodel/cover/Render2.glb", {
-    draco: true,
-  });
-  scene1.remove(scene1.children[0]);
-  const group = new THREE.Group();
-  group.add(nodes.Carcasa_basica037);
-  group.add(nodes.Carcasa_basica037_1);
-  group.add(nodes.Carcasa_basica037_2);
-  scene1.add(group);
-  //console.log(nodes);
-  console.log(group);
-  part1.value = nodes.Carcasa_basica037;
-}
-
-function handler3() {
-  scene1.remove(scene1.children[0]);
-  const group = new THREE.Group();
-  console.log(toRaw(part1.value));
-  group.add(toRaw(part1.value));
-  group.add(toRaw(part2.value));
-  group.add(toRaw(part3.value));
-  scene1.add(group);
-}
-
-function handler4() {
+function changeColor() {
   scene1.remove(scene1.children[0]);
   const group = new THREE.Group();
   const meshCover = toRaw(part1.value);
@@ -62,17 +16,6 @@ function handler4() {
   var color = new THREE.Color(Math.random(), Math.random(), Math.random());
   var material = new THREE.MeshBasicMaterial({ color: color });
   meshCover.material = material;
-  group.add(meshCover);
-  group.add(toRaw(part2.value));
-  group.add(toRaw(part3.value));
-  scene1.add(group);
-}
-
-function handler5() {
-  scene1.remove(scene1.children[0]);
-  const group = new THREE.Group();
-  const meshCover = toRaw(part1.value);
-  meshCover.material = toRaw(mat3.value);
   group.add(meshCover);
   group.add(toRaw(part2.value));
   group.add(toRaw(part3.value));
@@ -142,46 +85,47 @@ onMounted(async () => {
       <TresAmbientLight :intensity="1" />
     </TresCanvas>
   </div>
-  <div class="flex gap-2 mt-3">
-    <div @click="handler">111</div>
-    <div @click="handler2">222</div>
-    <div @click="handler3">333</div>
-    <div @click="handler4">444</div>
-    <div @click="handler5">555</div>
-  </div>
-  <div class="mt-3">
-    <div class="font-semibold">Cover</div>
-    <div class="flex gap-2">
-      <div
-        @click="changeMaterialCover(item)"
-        v-for="item in Object.keys(mats)"
-        :key="item"
-      >
-        {{ item }}
+  <div class="px-6">
+    <div class="flex gap-2 mt-3">
+      <div @click="changeColor" class="cursor-pointer">Change color</div>
+    </div>
+    <div class="mt-3">
+      <div class="font-semibold">Cover</div>
+      <div class="flex gap-2">
+        <div
+          @click="changeMaterialCover(item)"
+          v-for="item in Object.keys(mats)"
+          :key="item"
+          class="cursor-pointer"
+        >
+          {{ item }}
+        </div>
       </div>
     </div>
-  </div>
-  <div class="mt-3">
-    <div class="font-semibold">Botones</div>
-    <div class="flex gap-2 mt-3">
-      <div
-        @click="changeMaterialButtons(item)"
-        v-for="item in Object.keys(mats)"
-        :key="item"
-      >
-        {{ item }}
+    <div class="mt-3">
+      <div class="font-semibold">Botones</div>
+      <div class="flex gap-2 mt-3">
+        <div
+          @click="changeMaterialButtons(item)"
+          v-for="item in Object.keys(mats)"
+          :key="item"
+          class="cursor-pointer"
+        >
+          {{ item }}
+        </div>
       </div>
     </div>
-  </div>
-  <div class="mt-3">
-    <div class="font-semibold">Camara</div>
-    <div class="flex gap-2 mt-3">
-      <div
-        @click="changeMaterialCameraHole(item)"
-        v-for="item in Object.keys(mats)"
-        :key="item"
-      >
-        {{ item }}
+    <div class="mt-3">
+      <div class="font-semibold">Camara</div>
+      <div class="flex gap-2 mt-3">
+        <div
+          @click="changeMaterialCameraHole(item)"
+          v-for="item in Object.keys(mats)"
+          :key="item"
+          class="cursor-pointer"
+        >
+          {{ item }}
+        </div>
       </div>
     </div>
   </div>
